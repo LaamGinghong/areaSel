8a7467bc261e46151fe25b6f27ece271b7b89432
LaamGinghong <lanjinghang@banggood.com>
2018-07-20 11:45:36 +0800

    添加省级组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {ProvinceComponent} from './province/province.component';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProvinceComponent
   ],
   imports: [
     BrowserModule,
